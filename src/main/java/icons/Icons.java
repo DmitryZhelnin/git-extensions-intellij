d2343a4f1258bd4f0143446fce36b0c819118a1d
dzhelnin <mitya_zh@mail.ru>
2019-01-08 16:43:29 +0300

    load icons by IconLoader

---
+++ src/main/java/icons/Icons.java
@@ -0,0 +1,32 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface Icons {
+        Icon DIFF = IconLoader.getIcon("/icons/Diff.png");
+        Icon FILE_HISTORY = IconLoader.getIcon("/icons/FileHistory.png");
+        Icon RESET = IconLoader.getIcon("/icons/ResetWorkingDirChanges.png");
+        Icon BROWSE = IconLoader.getIcon("/icons/BrowseFileExplorer.png");
+        Icon CLONE = IconLoader.getIcon("/icons/CloneRepoGit.png");
+        Icon INIT = IconLoader.getIcon("/icons/star.png");
+        Icon COMMIT = IconLoader.getIcon("/icons/RepoStateClean.png");
+        Icon PULL = IconLoader.getIcon("/icons/Pull.png");
+        Icon PUSH = IconLoader.getIcon("/icons/Push.png");
+        Icon STASH = IconLoader.getIcon("/icons/stash.png");
+        Icon MANAGE_REMOTES = IconLoader.getIcon("/icons/Remotes.png");
+        Icon EDIT_GITIGNORE = IconLoader.getIcon("/icons/EditGitIgnore.png");
+        Icon APPLY_PATCH = IconLoader.getIcon("/icons/PatchApply.png");
+        Icon FORMAT_PATCH = IconLoader.getIcon("/icons/PatchFormat.png");
+        Icon BLAME = IconLoader.getIcon("/icons/Blame.png");
+        Icon CHECKOUT = IconLoader.getIcon("/icons/checkout.png");
+        Icon CREATE_BRANCH = IconLoader.getIcon("/icons/BranchCreate.png");
+        Icon MERGE = IconLoader.getIcon("/icons/Merge.png");
+        Icon REBASE = IconLoader.getIcon("/icons/Rebase.png");
+        Icon CONFLICT = IconLoader.getIcon("/icons/Conflict.png");
+        Icon CHERRY = IconLoader.getIcon("/icons/CherryPick.png");
+        Icon GITBASH = IconLoader.getIcon("/icons/GitForWindows.png");
+        Icon SETTINGS = IconLoader.getIcon("/icons/Settings.png");
+        Icon INFO = IconLoader.getIcon("/icons/information.png");
+}
