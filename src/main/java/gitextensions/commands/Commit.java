ae9740bf37b91bb6cc3497aa5f949e159981a149
Dmitry Zhelnin <mitya_zh@mail.ru>
2019-01-13 17:36:57 +0300

    Add BranchNameService to help to display branch name on the Commit button

--- src/main/java/gitextensions/commands/Commit.java
+++ src/main/java/gitextensions/commands/Commit.java
@@ -1,17 +1,52 @@
 package gitextensions.commands;
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.google.common.base.Strings;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ex.CustomComponentAction;
+import com.intellij.openapi.actionSystem.impl.ActionButtonWithText;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import gitextensions.BranchNameService;
 import org.jetbrains.annotations.NotNull;
 
-public class Commit extends BaseAction {
+import javax.swing.*;
+import java.util.Objects;
+
+public class Commit extends BaseAction implements CustomComponentAction {
+
+    private static final int BRANCH_CHECK_TIMEOUT = 1000;
+    private String lastBranchName;
+    private long lastCheckMillis;
+
     public Commit() {
         super(Commands.COMMIT);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        VirtualFile file = e.getData(PlatformDataKeys.VIRTUAL_FILE);
+        if (file == null || System.currentTimeMillis() - lastCheckMillis < BRANCH_CHECK_TIMEOUT) {
+            return;
+        }
+        String branchName = BranchNameService.getInstance().getBranchName(file);
+        if (!Objects.equals(lastBranchName, branchName)) {
+            Presentation presentation = e.getPresentation();
+            String text = Strings.isNullOrEmpty(branchName) ? "Commit" : String.format("Commit (%s)", branchName);
+            presentation.setText(text);
+            lastBranchName = branchName;
+        }
+        lastCheckMillis = System.currentTimeMillis();
     }
 
     @Override
     public void beforeActionPerformedUpdate(@NotNull AnActionEvent e) {
         FileDocumentManager.getInstance().saveAllDocuments();
         super.beforeActionPerformedUpdate(e);
+    }
+
+    @NotNull
+    @Override
+    public JComponent createCustomComponent(@NotNull Presentation presentation) {
+        return new ActionButtonWithText(this, presentation, ActionPlaces.UNKNOWN, ActionToolbar.DEFAULT_MINIMUM_BUTTON_SIZE);
     }
 }
