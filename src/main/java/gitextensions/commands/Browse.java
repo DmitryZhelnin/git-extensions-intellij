8b677574d3cf707873680f347ec366419e375c92
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-09-15 14:27:47 +0300

    fix(): Override getFileNameFromEvent in Browse command to allow get project base path if no current file

--- src/main/java/gitextensions/commands/Browse.java
+++ src/main/java/gitextensions/commands/Browse.java
@@ -1,7 +1,26 @@
 package gitextensions.commands;
 
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class Browse extends BaseAction {
     public Browse() {
         super(Commands.BROWSE);
+    }
+
+    @Nullable
+    @Override
+    protected String getFileNameFromEvent(@NotNull AnActionEvent e) {
+        String result = super.getFileNameFromEvent(e);
+        if (result == null) {
+            Project project = e.getData(PlatformDataKeys.PROJECT);
+            if (project != null) {
+                result = project.getBasePath();
+            }
+        }
+        return result;
     }
 }
